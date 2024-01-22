d89f3e7a00c5cdbe5f726bdd0d4f188f789fb781
ryosukeknd <75285941+ryosukeknd@users.noreply.github.com>
2024-01-22 18:04:56 +0900

    インスタンスIDの取得方法変更part2

--- test_web/main.py
+++ test_web/main.py
@@ -1,27 +1,9 @@
 from fastapi import FastAPI
 from fastapi.responses import PlainTextResponse
-import requests
+import os
 
 # インスタンスの情報取得
-INSTANCE_ID = "Unknown INSTANCE_ID"
-try:
-    resp = requests.put(
-        "http://169.254.169.254/latest/api/token",
-        headers={
-            "X-aws-ec2-metadata-token-ttl-seconds": 300
-        }
-    )
-    token = resp.text()
-
-    resp = requests.get(
-        "http://169.254.169.254/latest/meta-data/instance-id",
-        headers={
-            "X-aws-ec2-metadata-token": token
-        }
-    )
-    INSTANCE_ID = resp.text()
-except Exception:
-    pass
+INSTANCE_ID = os.environ.get("INSTANCE_ID", "INSTANCE_ID")
 
 
 app = FastAPI()
---
+++ test_web/run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+TOKEN=`curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600"`
+INSTANCE_ID=`curl -H "X-aws-ec2-metadata-token: $TOKEN" -s http://169.254.169.254/latest/meta-data/instance-id`
+echo ${INSTANCE_ID}
+
+export INSTANCE_ID
+
+uvicorm main:app --host=0.0.0.0 --port=80
