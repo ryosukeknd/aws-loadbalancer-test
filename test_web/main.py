02619ef56b52dcd0ece117cd76cc23fe2c5ce4dc
ryosukeknd <75285941+ryosukeknd@users.noreply.github.com>
2024-01-22 17:45:22 +0900

    インスタンスIDの取得方法変更

--- test_web/main.py
+++ test_web/main.py
@@ -5,8 +5,20 @@ import requests
 # インスタンスの情報取得
 INSTANCE_ID = "Unknown INSTANCE_ID"
 try:
-    url = "169.254.169.254/latest/meta-data/instance-id/"
-    resp = requests.get(url)
+    resp = requests.put(
+        "http://169.254.169.254/latest/api/token",
+        headers={
+            "X-aws-ec2-metadata-token-ttl-seconds": 300
+        }
+    )
+    token = resp.text()
+
+    resp = requests.get(
+        "http://169.254.169.254/latest/meta-data/instance-id",
+        headers={
+            "X-aws-ec2-metadata-token": token
+        }
+    )
     INSTANCE_ID = resp.text()
 except Exception:
     pass
